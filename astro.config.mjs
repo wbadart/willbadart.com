e445a0aba5034dad7daf7f8221db5d0b9de956a5
Will Badart <will@willbadart.com>
2025-11-04 12:23:31 -0700

    Get light/dark syntax highlighting up and running

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,15 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://willbadart.com',
   trailingSlash: 'always',
+  markdown: {
+    shikiConfig: {
+      themes: {
+        dark: 'github-dark',
+        light: 'github-light',
+      },
+      defaultColor: false,
+    },
+  },
   vite: {
     ssr: {
       noExternal: ['open-props'],
--- src/posts/asdf.md
+++ src/posts/asdf.md
@@ -5,3 +5,10 @@ published: 2025-11-04
 # Example post
 
 foo bar
+
+```haskell
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    quicksort (filter (<= x)) xs <> [x] <> quicksort (filter (> x) xs)
+```
